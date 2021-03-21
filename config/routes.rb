70b98925d54f38a2152c529dcf1c95ae7e60ddb3
xita Rps <xitarps@gmail.com>
2021-03-21 19:16:10 -0300

    Add friendship views

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
+  resources :user_stocks, only: [:create, :destroy]
+  
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-
-  resources :user_stocks, only: [:create, :destroy]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
